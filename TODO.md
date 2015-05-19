cac9b1244a731ea3d86ab4f9a26b57f9c8a49b10
Cinemapub Ubuntu <support@cinemapub.be>
2015-05-19 15:45:49 +0200

    * accepts user@host * accepts stdin commands

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# Additional development
+
+[X] support user@server notation
+[X] get commands from stdin
+[ ] save result to file(s)
+[ ] execute .sh script instead of just 1 line
+
--- multissh.sh
+++ multissh.sh
@@ -1,31 +1,45 @@
 #!/usr/bin/env bash
 set -e
 prog=$(basename $0)
+
+# print usage
 usage(){
-cat <<'EOT'
-$0 [options] [host1,host2,host3] [command]
-	runs [command] on all specified hosts via ssh
-[options]: 
-	-b : run in background (so +- simultaneously on all servers)
-	-u [user]	: use this user as default ssh login
-EOT
-exit 0;
+	echo "===================================================" >&2
+	echo "$prog [options] [host1,host2,host3] [command]" >&2
+	echo "  runs [command] on all specified hosts via ssh" >&2
+	echo "  [host] can be ip address, hostname or user@hostname" >&2
+	echo "  [options]:" >&2 
+	echo "    -b        : start in background (so +- simultaneously on all servers)" >&2
+	echo "    -u [user] : use this username for shh login (default: $USER)" >&2
+	exit
 }
 
 # exit if there are no arguments
 [ $# -eq 0 ] && usage
 
+# prefix output 
+prefix_feed(){
+	word=$1
+	gawkok=$(which gawk)
+	if [ -n "$gawkok" ] ; then 
+		# gawk is installed - we can use strftime
+		gawk "{ print strftime(\"[$*][%Y-%m-%d %T]\"), \$0; fflush(); }"
+	else
+		# no gawk installed - use awk
+		awk "{print \"[$*]\", \$0;}"
+	fi
+}
 # initialize default values
 background=0
 user=$(whoami)
 
 # parse options
 set -- `getopt -n$prog -u -a --longoptions "help" "hbu:" "$@"`
-
+defuser=$USER
 while [ $# -gt 0 ] ; do
 	case "$1" in
 		-b) background=1;;
-		-u) user=$2; shift;;
+		-u) defuser=$2; shift;;
 		-h| --help) usage;;
 		--) shift;break;;
 		*) break;;
@@ -36,29 +50,64 @@ done
 hostlist="$1"
 shift
 command="$*"
-
+echo -e "### STARTED" | prefix_feed $prog
+tmpcmd=""
 cleanup_before_exit () {
-# this code is run before exit
-echo -e " "
+	# this code is run before exit
+	if [ -f "$tmpcmd" ] ; then
+		rm -f "$tmpcmd"
+	fi
+	echo -e "### FINISHED" | prefix_feed $prog
 }
+
 # trap catches the exit signal and runs the specified function
 trap cleanup_before_exit EXIT
 
-# OK now do stuff
+# if necessary, read the sequence of commands first
+if [ "$command" == "-" ] ; then
+	# first read from stdin , then send it to all hosts
+	echo "### $prog: read commands from stdin" | prefix_feed stdin
+	tmpcmd="/tmp/$prog.$$.temp"
+	cat > $tmpcmd
+fi
+	
 hosts=$(echo $hostlist | tr "," "\n")
 for host in $hosts ; do
-	ip=$(ping -c 1 $host | awk 'NR == 1 {gsub("[\(\)]*",""); print $3}')
-	if [ "$host" == "$ip" ] ; then
-		echo -- execute as [$user] on [$host]
-	else 
-		echo -- execute as [$user] on [$host] - [$ip]
+	
+	# first check for user@host notation
+	user=$defuser
+	if [ "$host" != "$(echo $host | tr '@' ':')" ] ; then
+		## first parse username, if any
+		user=$(echo $host | cut -d@ -f1)
+		host=$(echo $host | cut -d@ -f2)
 	fi
+	
+	# now find IP address
+	ip=$(ping -c 1 $host | sed 's/[\(\)]*//g' | awk 'NR == 1 {print $3}')
+	if [ "$host" == "$ip" ] ; then
+		echo "### $prog: execute as [$user] on [$host]" | prefix_feed $host
+	else 
+		echo "### $prog: execute as [$user] on [$host] - [$ip]" | prefix_feed $host
+	fi
+	
+
 	if [ $background == 1 ]; then
-		(ssh $user@$host "$command" | sed "s/^/[$host]:/") &
+		# start in bg
+		if [ -n "$tmpcmd" ] ; then
+			(cat "$tmpcmd" | ssh $user@$host sh | prefix_feed $host) &
+		else
+			(ssh $user@$host "$command" | prefix_feed $host) &
+		fi
 	else
-		ssh $user@$host "$command"
+		# start in fg
+		if [ -n "$tmpcmd" ] ; then
+			cat "$tmpcmd" | ssh $user@$host sh | prefix_feed $host
+		else
+			ssh $user@$host "$command" | prefix_feed $host
+		fi
 	fi
 done
+sleep 1
 if [ $background == 1 ]; then
 	# will make sure 1st returned stdout will not come behind the prompt
 	(echo " ") & 
